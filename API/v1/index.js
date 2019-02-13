3c1a3bb68ba9319eb262d4507df3d8deae02a106
timi-codes <tejumoladavid@gmail.com>
2019-02-14 00:34:19 +0100

    add eslint

--- API/v1/index.js
+++ API/v1/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 7778;
 
-app.get("/", (req, res) => {
-  res.send("Api is working");
+app.get('/', (req, res) => {
+  res.send('Api is working');
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
