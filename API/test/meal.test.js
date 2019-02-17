968fe70177504f4194a8f814e885f301998e145a
timi-codes <tejumoladavid@gmail.com>
2019-02-17 17:54:27 +0100

    add POST /meals/ route test

--- API/test/meal.test.js
+++ API/test/meal.test.js
@@ -27,4 +27,49 @@ describe('Meal', () => {
         });
     });
   });
+
+  /**
+   * Test the POST /meals/ route
+   */
+  describe('POST /meals', () => {
+    it('it should not POST a meal without name field', (done) => {
+      const meal = {
+        size: 'Small',
+        price: '300',
+      };
+      chai
+        .request(app)
+        .post('/api/v1/meals')
+        .send(meal)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.should.have.property('status').eql('error');
+          res.body.should.have.property('message').eql('All parameters are required');
+          done();
+        });
+    });
+
+    it('it should post a meal', (done) => {
+      const meal = {
+        name: 'Porridge',
+        size: 'Small',
+        price: '300',
+      };
+      chai
+        .request(app)
+        .post('/api/v1/meals')
+        .send(meal)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Meal successfully added!');
+          res.body.data.should.have.property('id');
+          res.body.data.should.have.property('name');
+          res.body.data.should.have.property('size');
+          res.body.data.should.have.property('price');
+          done();
+        });
+    });
+  });
 });
