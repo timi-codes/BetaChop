937a064c6b4729adb6665fe0599565ae8d6d065d
timi-codes <tejumoladavid@gmail.com>
2019-02-17 17:48:18 +0100

    add GET /meals route test

--- API/test/meal.test.js
+++ API/test/meal.test.js
@@ -1,6 +1,30 @@
 import chai from 'chai';
+import 'chai/register-should';
 import chaiHttp from 'chai-http';
 import app from '../v1/index';
+import dummyData from '../v1/utils/dummyData';
 
+chai.use(chaiHttp);
 
-const should = chai.use(chaiHttp).should();
+// This runs before each test
+describe('Meal', () => {
+  beforeEach((done) => {
+    done();
+  });
+
+  /**
+   * Test the GET /meals/ route
+   */
+  describe('GET /meals', () => {
+    it('it should get all the meals', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/meals')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.data.should.be.a('array');
+          done();
+        });
+    });
+  });
+});
