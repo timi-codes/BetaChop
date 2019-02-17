d885e574b6ba5eebd436ae8ee662c0422330edb7
timi-codes <tejumoladavid@gmail.com>
2019-02-17 17:55:56 +0100

    add /meals/:id

--- API/test/meal.test.js
+++ API/test/meal.test.js
@@ -72,4 +72,51 @@ describe('Meal', () => {
         });
     });
   });
+
+  /**
+   * Test the GET /meals/:id route
+   */
+  describe('GET /meals/:id', () => {
+    it('it should GET a meal by the given id', (done) => {
+      const mealId = Number(dummyData.meals[0].id);
+      chai
+        .request(app)
+        .get(`/api/v1/meals/${mealId}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.data.should.have.property('id').eql(mealId);
+          res.body.data.should.have.property('name');
+          res.body.data.should.have.property('size');
+          res.body.data.should.have.property('price');
+          done();
+        });
+    });
+
+    it('it should not GET a meal that is not available', (done) => {
+      const mealId = 10000;
+      chai
+        .request(app)
+        .get(`/api/v1/meals/${mealId}`)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Meal cannot be found');
+          done();
+        });
+    });
+
+    it('it should throw an error when a number is not passed as id', (done) => {
+      const mealId = 'o';
+      chai
+        .request(app)
+        .get(`/api/v1/meals/${mealId}`)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Invalid ID. ID must be a number');
+          done();
+        });
+    });
+  });
 });
