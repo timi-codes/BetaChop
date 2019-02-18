f674176d0a1db416e5b348bedfffd1587c7d818c
timi-codes <tejumoladavid@gmail.com>
2019-02-18 11:07:42 +0100

    implement endpoint for posting order

---
+++ API/v1/services/menu.service.js
@@ -0,0 +1,21 @@
+import dummyData from '../utils/dummyData';
+import Menu from '../models/menu.model';
+import Meal from '../models/meal.model';
+
+/**
+ * menu services performs all action related to menu-
+ * fetch menu for the day and setup menu for the day
+ */
+export default class MenuService  {
+
+    const [Meal]
+  /**
+   * @description Setup the meal for the day
+   * @returns {Array} menu object array
+   */
+  setUpMenu(id) {
+
+  },
+};
+
+export default MenuService;
