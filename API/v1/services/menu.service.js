37fa73804d04a5011f43f5555ef94e919db15b9f
timi-codes <tejumoladavid@gmail.com>
2019-02-18 12:42:57 +0100

    add menu routes to get menu for the day

--- API/v1/services/menu.service.js
+++ API/v1/services/menu.service.js
@@ -6,15 +6,28 @@ import Meal from '../models/meal.model';
  * menu services performs all action related to menu-
  * fetch menu for the day and setup menu for the day
  */
-export default class MenuService  {
-
-    const [Meal]
+const MenuService = {
   /**
    * @description Setup the meal for the day
    * @returns {Array} menu object array
    */
   setUpMenu(id) {
+    const foundMeal = dummyData.meals.find(meal => meal.id === Number(id));
 
+    if (foundMeal) {
+      foundMeal.available = true;
+      dummyData.menu.push(foundMeal);
+    }
+
+    return foundMeal;
+  },
+
+  /**
+   * @description Retrieve and return all menu from our dummyy data
+   * @returns {Array} menu object array
+   */
+  fetchMenu() {
+    return dummyData.meal.find(meal => meal.available === true);
   },
 };
 
