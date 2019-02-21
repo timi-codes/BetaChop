3efb023b16f49e796925f4f253a4f9092d376439
timi-codes <tejumoladavid@gmail.com>
2019-02-22 00:03:23 +0100

    add babel build to packge.json

--- API/build/v1/utils/utility.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var Utility = {};
-var _default = Utility;
-exports.default = _default;
