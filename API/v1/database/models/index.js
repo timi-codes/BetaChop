1647c1124615273e6e2d5af86e9c56b172f9d52c
timi-codes <tejumoladavid@gmail.com>
2019-03-07 17:11:05 +0100

    heroku dev

--- API/v1/database/models/index.js
+++ API/v1/database/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.environment === 'production') {
-  sequelize = new Sequelize(process.env.HEROKU_URI, config.dialect);
+  sequelize = new Sequelize(process.env.DATABASE_URL, config.dialect);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
