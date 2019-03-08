f935b349e8fdfa85d27c3bdb293504a771cf5949
timi-codes <tejumoladavid@gmail.com>
2019-03-08 05:23:07 +0100

    heroku changes

--- API/v1/database/models/index.js
+++ API/v1/database/models/index.js
@@ -12,7 +12,7 @@ const db = {};
 let sequelize;
 console.log(env);
 if (config.environment === 'production') {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], { dialect: 'postgres' });
 } else {
   // sequelize = new Sequelize(
   //   'postgres://zdycbqstcpqddg:cbeeca55ddc249db336ebc9870b8260fb0c55a22fd366315fe406d01dfe0a54d@ec2-174-129-236-21.compute-1.amazonaws.com:5432/d7cdlo945mtmn3?ssl=true',
