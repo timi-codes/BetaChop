e0eaa1b45e97b445ec54952af483c9cb259b0e10
timi-codes <tejumoladavid@gmail.com>
2019-03-08 05:04:35 +0100

    heroku changes

--- API/v1/database/models/index.js
+++ API/v1/database/models/index.js
@@ -12,22 +12,7 @@ const db = {};
 let sequelize;
 console.log(env);
 if (env === 'production') {
-  sequelize = new Sequelize(
-    'postgres://zdycbqstcpqddg:cbeeca55ddc249db336ebc9870b8260fb0c55a22fd366315fe406d01dfe0a54d@ec2-174-129-236-21.compute-1.amazonaws.com:5432/d7cdlo945mtmn3?ssl=true',
-    {
-      dialect: 'postgres',
-      ssl: true,
-      port: 5432,
-      environment: 'production',
-      operatorsAliases: false,
-      dialectOption: {
-        ssl: true,
-      },
-      define: {
-        timestamps: false,
-      },
-    },
-  );
+  sequelize = new Sequelize(config.uri, config);
 } else {
   // sequelize = new Sequelize(
   //   'postgres://zdycbqstcpqddg:cbeeca55ddc249db336ebc9870b8260fb0c55a22fd366315fe406d01dfe0a54d@ec2-174-129-236-21.compute-1.amazonaws.com:5432/d7cdlo945mtmn3?ssl=true',
