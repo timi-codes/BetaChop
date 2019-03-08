78f02f21e57c1ea3716978e8fec56f64adc1309a
timi-codes <tejumoladavid@gmail.com>
2019-03-08 10:07:55 +0100

    heroku changes

--- API/v1/database/models/index.js
+++ API/v1/database/models/index.js
@@ -11,7 +11,7 @@ const db = {};
 let sequelize;
 console.log(env);
 if (env === 'production') {
-  sequelize = new Sequelize(process.env[config.environment], {
+  sequelize = new Sequelize(process.env[config.uri], {
     dialectOptions: {
       ssl: true,
       native: true,
