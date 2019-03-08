ed9b7042dea4f6e196a8bbd36a703b1df263befc
timi-codes <tejumoladavid@gmail.com>
2019-03-08 10:10:50 +0100

    heroku changes

--- dist/v1/database/models/index.js
+++ dist/v1/database/models/index.js
@@ -13,11 +13,9 @@ var _path = require("path");
 
 var _sequelize = _interopRequireDefault(require("sequelize"));
 
-var _pg = _interopRequireDefault(require("pg"));
-
 var _config = _interopRequireDefault(require("../../config/config"));
 
-_pg.default.defaults.ssl = true;
+// pg.defaults.ssl = true;
 var basename = (0, _path.basename)(__filename);
 var env = process.env.NODE_ENV || 'development';
 var config = _config.default[env];
@@ -25,39 +23,17 @@ var db = {};
 var sequelize;
 console.log(env);
 
-if (config.environment === 'production') {
-  // sequelize = new Sequelize(
-  //   'postgres://zdycbqstcpqddg:cbeeca55ddc249db336ebc9870b8260fb0c55a22fd366315fe406d01dfe0a54d@ec2-174-129-236-21.compute-1.amazonaws.com:5432/d7cdlo945mtmn3?ssl=true',
-  //   { dialect: 'postgres' },
-  // );
-  sequelize = new _sequelize.default('postgres://zdycbqstcpqddg:cbeeca55ddc249db336ebc9870b8260fb0c55a22fd366315fe406d01dfe0a54d@ec2-174-129-236-21.compute-1.amazonaws.com:5432/d7cdlo945mtmn3?ssl=true', {
-    dialect: 'postgres',
-    ssl: true,
-    port: 5432,
-    operatorsAliases: false,
-    dialectOption: {
-      ssl: true
-    },
-    define: {
-      timestamps: false
+if (env === 'production') {
+  sequelize = new _sequelize.default(process.env[config.uri], {
+    dialectOptions: {
+      ssl: true,
+      native: true
     }
   });
-} else {// sequelize = new Sequelize(
-  //   'postgres://zdycbqstcpqddg:cbeeca55ddc249db336ebc9870b8260fb0c55a22fd366315fe406d01dfe0a54d@ec2-174-129-236-21.compute-1.amazonaws.com:5432/d7cdlo945mtmn3?ssl=true',
-  //   {
-  //     dialect: 'postgres',
-  //     ssl: true,
-  //     port: 5432,
-  //     operatorsAliases: false,
-  //     dialectOption: {
-  //       ssl: true,
-  //     },
-  //     define: {
-  //       timestamps: false,
-  //     },
-  //   },
-  // );
-}
+} // else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
 
 (0, _fs.readdirSync)(__dirname).filter(function (file) {
   return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
