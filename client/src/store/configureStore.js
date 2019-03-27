baeaa61c9260b4547a577f634f54657ab0bcfe00
timi-codes <tejumoladavid@gmail.com>
2019-03-27 23:42:45 +0100

    add login saga

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,5 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
+const initialiseSagaMiddleware = createSagaMiddleware();
+
+// eslint-disable-next-line no-underscore-dangle
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  storeEnhancers(applyMiddleware(thunk, initialiseSagaMiddleware)),
+);
+
 export default store;
