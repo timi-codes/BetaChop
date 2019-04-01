ffff3d8330e45941b033c7a7445389bb5039b034
timi-codes <tejumoladavid@gmail.com>
2019-04-01 12:37:47 +0100

    test authentication saga generators

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,10 +2,9 @@ import isEmpty from 'lodash/isEmpty';
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    isLoginPending: true,
-    isAuthenticated:false,
-    user:{}
-  }
+  isLoginPending: true,
+  isAuthenticated: false,
+  user: {},
+};
 
 const { SET_CURRENT_USER, UPDATE_PROFILE } = actionType;
-export const
